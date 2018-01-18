388b4b12253d97d454d49c120ba16e93e76464a4
Roman <roman.lossin@icloud.com>
2018-01-18 21:05:31 +0100

    - ADD: Added French translation (thanks to the great community!)

---
+++ languages/fr_FR/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Exitintent Settings' => 'Paramètres d\'Exitintent',
+    'Pages'               => 'Les Pages',
+    'HTML'                => 'HTML',
+    'Sensitivity'         => 'Sensibilité',
+    'Aggressive'          => 'Agressif',
+    'Timer'               => 'Heures',
+    'Cookie Lifetime'     => 'Durée de vie du cookie',
+    'Days'                => 'Jours',
+    'Cookie Name'         => 'Nom du cookie',
+    'Sitewide Cookie'     => 'Cookie à l\'échelle du site',
+    'Save'                => 'Sauvegarder',
+];
