43a5ca6f585cc34c2e0eb670ce4064ac106757f4
Roman <roman.lossin@icloud.com>
2018-01-18 21:05:15 +0100

    - ADD: Replaced bower with npm/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
-module.exports = [
-    {
-        entry: {
-			"exitintent-settings": "./app/components/exitintent-settings.vue"
-        },
-        output: {
-            filename: "./app/bundle/[name].js"
-        },
-        module: {
-            loaders: [
-                {test: /\.vue$/, loader: "vue"}
-            ]
-        }
-    }
+const CopyWebpackPlugin = require ('copy-webpack-plugin');
+const path = require ('path');
 
+
+module.exports = [
+	{
+		entry: {
+			"exitintent-settings": "./app/components/exitintent-settings.vue"
+		},
+		output: {
+			filename: "./app/bundle/[name].js"
+		},
+		plugins: [
+			new CopyWebpackPlugin ([
+				{
+					from: './node_modules/ouibounce/build',
+					to: './app/assets/ouibounce'
+				}
+			], {
+				ignore: [
+					'*.txt'
+				],
+				copyUnmodified: true
+			})
+		],
+		module: {
+			loaders: [
+				{test: /\.vue$/, loader: "vue"}
+			]
+		}
+	}
 ];
